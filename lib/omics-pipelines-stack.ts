957e7e9d33165d9b8e65228be8a4f98dbf2834ae
Luis Gomez <lgg@amazon.com>
2023-10-11 15:07:21 +0200

    First version using cdk iac

---
+++ lib/omics-pipelines-stack.ts
@@ -0,0 +1,157 @@
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codecommit from 'aws-cdk-lib/aws-codecommit';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codedeploy from 'aws-cdk-lib/aws-codedeploy';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ecspatterns from 'aws-cdk-lib/aws-ecs-patterns';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as path from 'path';
+
+
+export class OmicsPipelinesStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // Git repositories
+
+    const workflowsCodeRepo = new codecommit.Repository(this, 'workflows_code_git', {
+      repositoryName: 'healthomics-workflows',
+      code: codecommit.Code.fromDirectory(path.join(__dirname, '../project/'), 'main'),
+    })
+
+    const workflowsCicdRepo = new codecommit.Repository(this, 'workflows_cicd_git', {
+      repositoryName: 'healthomics-cicd',
+      code: codecommit.Code.fromDirectory(path.join(__dirname, '../project/cicd'), 'main'),
+    })
+
+    // ECR Repository
+
+    const ecrRepo = new ecr.Repository(this, 'healthomics_ecr', {
+      repositoryName: 'healthomics-repo',
+      removalPolicy: RemovalPolicy.DESTROY, //change if you want to retain the repo
+    });
+
+    // IAM Roles
+
+    const codeDeployRole = new iam.Role(this, 'codeDeployRole', {
+      assumedBy: new iam.ServicePrincipal('codedeploy.amazonaws.com'),
+      description: 'CodeDeploy standard Role',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryFullAccess"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AWSCodeDeployRoleForECS"),
+      ],
+    });
+
+    const codeBuildRole = new iam.Role(this, 'codeBuildRole', {
+      assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
+      description: 'CodeBuild standard Role',
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ContainerRegistryFullAccess"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonOmicsFullAccess"),
+      ],
+    });
+
+
+    // CodeBuild projects
+
+    const buildProject = new codebuild.PipelineProject(this, 'build_project', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('cicd/buildspec-build.yaml'),
+      role: codeBuildRole,
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:3.0'),
+        privileged: true, // needed to connect to the Docker daemon for building the image
+      },
+    });
+
+    const testProject = new codebuild.PipelineProject(this, 'test_project', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('cicd/buildspec-test.yaml'),
+      role: codeBuildRole,
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:3.0'),
+        privileged: true, // needed to connect to the Docker daemon for building the image
+      },
+    });
+
+    const uploadProject = new codebuild.PipelineProject(this, 'upload_project', {
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('cicd/buildspec-upload.yaml'),
+      role: codeBuildRole,
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:3.0'),
+        privileged: true, // needed to connect to the Docker daemon for building the image
+      },
+    });
+
+
+    //// Build Pipeline
+
+    const buildPipeline = new codepipeline.Pipeline(this, 'workflows_build_pipeline', {
+      crossAccountKeys: false, // so no AWS KMS CMK is created
+      pipelineName: 'Build',
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    
+    // Build pipeline stages
+
+    // Build pipeline source stage
+
+    const sourceStage = buildPipeline.addStage({
+      stageName: 'Source',
+    });
+
+    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
+      actionName: 'CodeCommit',
+      repository: workflowsCodeRepo,
+      output: sourceOutput,
+      branch: 'main',
+      trigger: codepipeline_actions.CodeCommitTrigger.POLL, // disable CloudWatch events
+    });
+
+    sourceStage.addAction(sourceAction);
+    
+    // Build pipeline build stage
+
+    const buildStage = buildPipeline.addStage({
+      stageName: 'Build',
+    });
+
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'workflows_build_action',
+      project: buildProject,
+      input: sourceOutput,
+      outputs: [new codepipeline.Artifact()], // optional
+      executeBatchBuild: false, // optional, defaults to false
+      combineBatchBuildArtifacts: false, // optional, defaults to false
+      //role: role with permission to CLoudWatch Logs, CodeBuild, S3, ADD ECR
+      //arn:aws:iam::095077925459:role/AwsCicdStack-buildprojectRole8A6CB32D-RWLQGLB1AKC7
+    });
+
+    buildStage.addAction(buildAction);
+
+    // Build pipeline test stage
+
+    const testStage = buildPipeline.addStage({
+      stageName: 'Test',
+    });
+
+    const testAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'workflows_test_action',
+      project: testProject,
+      input: sourceOutput,
+      outputs: [new codepipeline.Artifact()], // optional
+      executeBatchBuild: false, // optional, defaults to false
+      combineBatchBuildArtifacts: false, // optional, defaults to false
+      //role: role with permission to CLoudWatch Logs, CodeBuild, S3, ADD ECR
+    });
+
+    testStage.addAction(testAction);
+
+  }
+
+}
