caeb298761d0b1fa081d1a7308402a7f37dcfa23
Luis Gomez <lgg@amazon.com>
2023-10-25 16:05:33 +0200

    fix omics conf setup during builds

--- project/cicd/scripts/importimages.sh
+++ project/cicd/scripts/importimages.sh
@@ -8,25 +8,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $BASEDIR
 git clone https://github.com/aws-samples/amazon-omics-tutorials.git
 HAVEHELPER=$(aws cloudformation list-stacks --output text | grep CREATE_COMPLETE | grep OmxEcrHelper-ContainerBuilder | wc -l)
-if [[ "${HAVEHELPER}" -gt 0 ]]
-then
-    echo "Installing omics helper..."
-    npm install aws-cdk -g
-    alias cdk='npx aws-cdk'
-    
-    npm install ts-node -g
-    npm install aws-cdk-lib -g
-    npm install fs -g
-    npm update -g aws-cdk
+# Skip this installation check,
+#  cdk is idempotent here and  this way we could keep the helper up to date
+echo "Installing omics helper..."
+npm install aws-cdk -g
+alias cdk='npx aws-cdk'
 
-    cd amazon-omics-tutorials
-    git pull origin
-    cd utils/cdk/omx-ecr-helper
-    npm install
-    cdk deploy --all --require-approval never
-else
-    echo "Omics helper already installed; ignoring..."
-fi
+npm install ts-node -g
+npm install aws-cdk-lib -g
+npm install fs -g
+npm update -g aws-cdk
+
+cd amazon-omics-tutorials
+git pull origin
+cd utils/cdk/omx-ecr-helper
+npm install
+cdk deploy --all --require-approval never
 
 # Look for public image references in workflow, generate manifests
 cd $WFDIR
@@ -34,8 +31,8 @@ mkdir -p $WFDIR/conf
 python3 $BASEDIR/amazon-omics-tutorials/utils/scripts/inspect_nf.py \
     -n public_registry_properties.json \
     -s container_substitutions.json \
-    --output-config-file conf/omics.config \
-    --output-manifest-file container_pull_manifest.json \
+    --output-config-file $WFDIR/conf/omics.config \
+    --output-manifest-file $WFDIR/container_pull_manifest.json \
     .
 # Use helper to upload images to ECR
 aws stepfunctions start-execution \
@@ -43,8 +40,8 @@ aws stepfunctions start-execution \
     --input file://container_pull_manifest.json
 
 # Include omics.config in workflow config
-HAVECONFIG=$(grep "omics.config" $WFDIR/project/workflow/nextflow-rnaseq/nextflow.config  | wc -l)
-if [[ "${HAVECONFIG}" -gt 0 ]]
+HAVECONFIG=$(grep "omics.config" $WFDIR/nextflow.config  | wc -l)
+if [[ "${HAVECONFIG}" -eq0 ]]
 then
     echo "includeConfig 'conf/omics.config'" >> $WFDIR/nextflow.config
 fi
