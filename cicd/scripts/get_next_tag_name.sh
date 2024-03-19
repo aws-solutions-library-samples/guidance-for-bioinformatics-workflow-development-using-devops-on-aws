4dbc270482779532a95d8d9e7e6a09ca28d48a4d
Luis Gomez <lgg@amazon.com>
2024-03-19 14:31:40 +0100

    Add build version to have 4-level workflow version

--- cicd/scripts/get_next_tag_name.sh
+++ cicd/scripts/get_next_tag_name.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
-# patch version from arguments
-LASTPATCHVER=$1
-NEWPATCHVER=$((LASTPATCHVER+1))
-
+# BUILD version from arguments
 # Get current branch name
 CURRBRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [ ! -z ${NEWPATCHVER}  ] && [ ! -z ${MAJORV} ] && [ ! -z ${MINORV} ] && [ ! -z ${CODEBUILD_RESOLVED_SOURCE_VERSION} ]
+LASTAG=$(git tag --merged ${CURRBRANCH})
+if [ -z ${LASTAG} ]
 then
-      TAGNAME="${CURRBRANCH}-${MAJORV}.${MINORV}.${NEWPATCHVER}"
+      LASTBUILDVER=0
+else
+      LASTBUILDVER="${LASTAG##*.}"
+fi
+
+# Generate new build version
+NEWBUILDVER=$((LASTBUILDVER+1))
+
+if [ ! -z ${NEWBUILDVER}  ] && [ ! -z ${MAJORV} ] && [ ! -z ${MINORV} ] && [ ! -z ${PATCHV} ] && [ ! -z ${CODEBUILD_RESOLVED_SOURCE_VERSION} ]
+then
+      TAGNAME="${CURRBRANCH}-${MAJORV}.${MINORV}.${PATCHV}.${NEWBUILDVER}"
       echo "${TAGNAME}"
 else
       exit 1
---
+++ cicd/scripts/set_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Script to set the environment variables for the workflow version
+# This script takes information from the workflow manifest and sets the environment variables accordingly
+# It currently only supports the version pattern Major.minor.patch from nextflow.config manifest
+# If the pattern is not found, the script exits with an error
+# TODO: Add support for other workflow languages (WDL, CWL)
+
+export MANIFESTVER=$(grep -E "\s+version\s+=\s*'.*?'" $WFDIR/nextflow.config | cut -d\' -f2)
+if [[ "$MANIFESTVER" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    export MAJORV=$(echo ${MANIFESTVER} | cut -d. -f1)
+    export MINORV=$(echo ${MANIFESTVER} | cut -d. -f2)
+    export PATCHV=$(echo ${MANIFESTVER} | cut -d. -f3)
+else
+    echo "Version in workflow manifest ${MANIFESTVER} doesn't match Major.minor.patch pattern."
+    exit 1
+fi
